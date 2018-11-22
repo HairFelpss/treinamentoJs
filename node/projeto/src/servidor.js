a1cb87006b9bf9613ffa021c6af6565e22819630
Felipe Alves <fajreyy.bh@gmail.com>
2018-11-22 15:41:20 -0200

    commit

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,15 +2,27 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/produtos', (req, res, next)=> {
-    console.log('Middleware 1...')
-    next()
+    res.send(bancoDeDados.getProdutos()) //converter para json automaticamente pelo método send
 })
 
-app.get('/produtos', (req, res, next)=> {
-    res.send({nome: 'Notebook', preco: 123.45}) //converter para json automaticamente pelo método send
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
 })
+
+app.post('/produtos', (req, res) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
 
 app.listen(porta, ()=> {
     console.log(`Servidor está executando na porta ${porta}.`)
